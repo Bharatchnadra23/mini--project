6a539a8f1f52a024238c53fb3d08cb64fa29e85d
Bharatchnadra23 <gorla.bharat9@gmail.com>
2025-03-07 16:58:36 +0530

    Your commit message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [CommonModule, FlexLayoutModule], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'responsive-layout-app';
+  title = 'Responsive Layout App';
 }
